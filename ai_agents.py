151e6b7794f43a9f72375b9bc0c6adf43cf7d111
TDK-99 <vale.99.mezzo@gmail.com>
2026-08-05 00:04:12 +0200

    improve system promnt in agentic summarize

--- ai_agents.py
+++ ai_agents.py
@@ -4,15 +4,14 @@ from schedule import jobs
 from dotenv import load_dotenv
 import os
 
-def agentic_summirize(jobs):
+def agentic_summarize(jobs):
     load_dotenv("your_cv_config/file_config.env")
     
     groq_client = Groq(os.getenv("LLM_KEY"))
     
     for index, row in jobs.iterrows():
-        istruzioni = "summarize the job description max 100 words,return only the summary without any other text or explanation"
-        messages = [{"role": "system", "content": istruzioni}] + [{"role": "user", "content": f"job description: {row['description']}"}]
-        response = groq_client.chat.completions.create(model="llama-3.1-8b-instant", messages=messages, temperature=0)
+        system_prompt = "summarize the job description max 100 words,return only the summary without any other text or explanation, important include if is remote or hydrid work, and the link for apply"
+        messages = [{"role": "system", "content": system_prompt}] + [{"role": "user", "content": f"job description: {row['description']}"}]
         result = response.choices[0].message.content
         jobs.at[index, "summary"] = result
     
