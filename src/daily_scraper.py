716e5f458836609c87746594fad749d5f4daeb3e
TDK-99 <vale.99.mezzo@gmail.com>
2026-08-21 21:01:53 +0200

    modify: delete the search remote work in scapper it  ll be manage from agent

--- src/daily_scraper.py
+++ src/daily_scraper.py
@@ -14,22 +14,5 @@ def job_scraper():
         linkedin_fetch_description= os.getenv("linkedin_fetch_description") == "True", # gets more info such as description, direct job url (slower)
         proxies= os.getenv("proxies").split(",") if os.getenv("proxies") else None,
     )
-    keywords = os.getenv("keyword_filter_remote_job", "").split(",")
 
-    keywords = [kw.strip().lower() for kw in keywords if kw.strip()]
-
-    def find_keyword(text):
-        if pd.isna(text):
-            return None
-        text = text.lower()
-        for kw in keywords:
-            if kw in text:
-                return kw
-        return None
-
-    jobs["work_mode"] = jobs["description"].apply(find_keyword)
-    if os.getenv("work_from_home") == "True":
-        df_filtered = jobs[jobs["work_mode"].notna()]
-    else:
-        df_filtered = jobs
-    return df_filtered
+    return jobs
