77a119881ff8770533d0961300a314d906b39523
TDK-99 <vale.99.mezzo@gmail.com>
2026-08-23 03:19:21 +0200

    feat: add the def ocherstrator main

--- main.py
+++ main.py
@@ -19,3 +19,16 @@ sys.path.append("..")
 from src.daily_scraper import job_scraper
 from src.ai_agents import agentic_summarize,agentic_analyze
 from src.smtp import send_email
+
+def main():
+    jobs= job_scraper()
+
+    jobs= agentic_summarize(jobs)
+
+    jobs= agentic_analyze(jobs)
+
+    send_email(jobs)
+
+
+
+main()
