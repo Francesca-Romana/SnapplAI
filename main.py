3f5ba1eb163fed4755845589eca28d9d44e50619
TDK-99 <vale.99.mezzo@gmail.com>
2026-08-23 02:49:36 +0200

    feat: add all import the py def in main

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ from dotenv import load_dotenv
 import pandas as pd
 from groq import Groq
 import json
+
+import sys
+sys.path.append("..")
+
+from src.daily_scraper import job_scraper
+from src.ai_agents import agentic_summarize,agentic_analyzent
+from src.smtp import send_email
